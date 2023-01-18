8d17cb30eab791c94caf7e91228f06ffd0c87e7b
fajar <frizky861@gmail.com>
2023-01-18 16:27:56 +0700

    feat : add image

--- features/posting/data/model.go
+++ features/posting/data/model.go
@@ -29,8 +29,29 @@ type PostUser struct {
 }
 
 func CoreToData(data posting.Core) Posting {
+	// image := []Image{}
 	return Posting{
 		Model:     gorm.Model{ID: data.ID},
 		Postingan: data.Postingan,
 	}
+}
+func coredata(data posting.Core) Image {
+	return Image{
+		PostID:    data.ID,
+		Image_url: data.Image_url,
+	}
+
+}
+func ToCoreSlice(data []posting.Core) []Image {
+	images := []Image{}
+	for _, v := range data {
+		image := Image{}
+		image.PostID = v.ID
+		image.Image_url = v.Image_url
+
+		images = append(images, image)
+	}
+
+	return images
+
 }
