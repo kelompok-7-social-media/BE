c87f84b99cfcef1c0ff385c1794692caf0c668f9
Herdiladania <herdiladania11@gmail.com>
2023-01-19 02:56:01 +0700

    feat : add response for listbooks

--- features/posting/handler/response.go
+++ features/posting/handler/response.go
@@ -42,18 +42,18 @@ func ToResponse(feature string, posting posting.Core) interface{} {
 	}
 }
 
-func ListBookCoreToBookRespon(dataCore posting.Core) PostingResponse { // data user core yang ada di controller yang memanggil user repository
+func ListPostCoreToPostRespon(dataCore posting.Core) PostingResponse { // data user core yang ada di controller yang memanggil user repository
 	return PostingResponse{
 		ID:        dataCore.ID,
 		Postingan: dataCore.Postingan,
 		UserName:  dataCore.UserName,
 	}
 }
-func ListBookCoreToBooksRespon(dataCore []posting.Core) []PostingResponse {
+func ListPostCoreToPostsRespon(dataCore []posting.Core) []PostingResponse {
 	var ResponData []PostingResponse
 
 	for _, value := range dataCore {
-		ResponData = append(ResponData, ListBookCoreToBookRespon(value))
+		ResponData = append(ResponData, ListPostCoreToPostRespon(value))
 	}
 	return ResponData
 }
